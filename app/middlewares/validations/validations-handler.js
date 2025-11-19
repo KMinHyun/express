e2fd7ac25a904e5a4038192a1414f1e81637ba96
KMinHyun <kimdennis264@gmail.com>
2025-11-19 14:43:31 +0900

    251119 validation-handler 주석 수정

--- app/middlewares/validations/validations-handler.js
+++ app/middlewares/validations/validations-handler.js
@@ -1,8 +1,8 @@
 import { validationResult } from "express-validator";
 
 export default function validatorHandler(request, response, next) {
-  // validationResult(request) : request에 담긴 유효성 검사 결과중, 에러를 모아서 배열로 반환
-  const errors = validationResult(request); // < request의 validation에서 체크한 오류를 모아서 배열로 반환. 에러가 있으면 반환, 없으면 빈 배열.
+  // validationResult(request) : request에 담긴 유효성 검사 결과중, 에러를 모아서 객체로 반환
+  const errors = validationResult(request); // <= request의 validation에서 체크한 오류를 모아서 객체로 반환. 에러가 있으면 반환, 없으면 빈 객체.
 
   if(!errors.isEmpty()) {
     const customErrors = errors.formatWith(error => `${error.path}: ${error.msg}`);
